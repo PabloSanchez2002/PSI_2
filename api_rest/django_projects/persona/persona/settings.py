1f0f7aa65eab7cd26dab24edc3fd84169d40537e
Javojav <javipelema@gmail.com>
2023-03-16 12:26:57 +0100

    git subir

--- api_rest/django_projects/persona/persona/settings.py
+++ api_rest/django_projects/persona/persona/settings.py
@@ -16,8 +16,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-STATIC_URL = '/'
-STATIC_ROOT = '/'
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-rpny+ths*337yn)a!clsfgoc*p3bqw0m1ndn_z!0hfa=$2z7*l
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    'https://psiapirest.onrender.com',
+    'psiapirest.onrender.com',
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
